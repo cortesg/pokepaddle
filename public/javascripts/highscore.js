31139a63ad38fe0cf06466d07a0395085d4ca24d
Gino Cortes <gino.angelo.cortes@gmail.com>
2016-08-18 16:52:16 -0400

    person name prompt if new user

--- public/javascripts/highscore.js
+++ public/javascripts/highscore.js
@@ -26,11 +26,14 @@ function decimalAdjust(type, value, exp) {
     };
 
 //getting the value of each cookie and assigning them to variables
+var person_var = Cookies.get("name_cookie")
 var score_var = Cookies.get("score_cookie")
 var time_var = Cookies.get("fastest_time_cookie");
 
 //for first-time users; if the cookies don't exist, your high score is 0 and your fastest time is an arbitrary high number
-if (score_var == null && time_var == null) {
+if (person_var == null && score_var == null && time_var == null) {
+    var person = prompt("Please enter your first name", "Ash");
+    Cookies.set("person_cookie", person)
     Cookies.set("score_cookie", "0");
     Cookies.set("fastest_time_cookie", "99999");
     $("#high_score").html("High Score: 0")
